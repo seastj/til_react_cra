9b9ea0285683e5d704cb22a4a9ec5c4e1d8f0439
seastj <dev.seastj@gmail.com>
2025-07-04 18:03:43 +0900

    [docs] emotion 기초

---
+++ src/components/ui/Toast.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+import React, { Children } from "react";
+
+function Toast({ message = "Please Message", bg = "#ccc" }) {
+  // js 자리
+  const StyledToast = styled.div`
+    z-index: 99999;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    background-color: ${props => props.bg};
+    color: #fff;
+    padding: 12px 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+  `;
+
+  // jsx 자리
+  return <StyledToast bg={bg}>{message}</StyledToast>;
+}
+
+export default Toast;
